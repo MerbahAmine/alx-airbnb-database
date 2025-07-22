af6fe5ee28219499130dc179ac7b16c6ccbb5a92
MerbahAmine <63507867+MerbahAmine@users.noreply.github.com>
2025-07-22 09:14:19 +0100

    joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,16 @@
-
 SELECT Booking.start_date Booking.end_date User.first_name  User.last_name User.email  User.phone_number
 FROM Booking 
 INNER JOIN User 
 ON Booking. booking_id = User.user_id;
+
+SELECT * 
+FROM Property
+LEFT JOIN Reviews
+On Property.property_id = Review.review_id;
+
+
+SELECT * 
+FROM User
+FULL OUTER JOIN Booking
+  On Booking. booking_id = User.user_id;
+
